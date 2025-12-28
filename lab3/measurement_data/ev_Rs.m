3bf06c86a3aaeae66b7539c09a919617810ef2f9
s-overflow <polt.simon@gmx.at>
2025-12-28 16:18:27 +0100

    finished evaluation of stator resistance

--- lab3/measurement_data/ev_Rs.m
+++ lab3/measurement_data/ev_Rs.m
@@ -1,7 +1,6 @@
 %14.12.2023 Gr. 14h
 %
 
-
 fname='statorR.txt';
 
 
@@ -14,28 +13,82 @@ mw=load(fname);
 
 U123=mw(:,[4 5 6]);
 I123=mw(:,[7 8 9]);
-indsNaN=find(abs(I123)<1);
-
-R123=U123./I123;
-R123_=R123;
-R123_(indsNaN)=NaN;
-% res=fun_grouping([U I P fs],1,0.03,5,0,100);
 
 
-figure(200);
-%plot(res.avg(:,2),res.avg(:,1),'-bo');
-plot(R123_,'o-');
-grid on; zoom on;
-%%
-Rtemp=R123_(:);
-Rtemp(indsNaN)=[];
-Rs=mean(Rtemp);
 
-figure(201);
-plot(I123,'o-');
+res=fun_grouping(U123(:,1),1,0.03,3,0,99);
 
-figure(202);
-plot(U123,'o-');
+
+U123_ = U123(res.ids, :);
+I123_ = I123(res.ids, :);
+indsNaN=find(abs(I123_)<1);
+R123=U123_./I123_;
+R123(indsNaN)=NaN;
+close all;
+
+styles = ["r", "b", "g"];
+
+fig = figure(101);
+fig.Position = [100 100 800 600]; 
+hold on;
+for i = 1:3
+    plot(U123(:,i),  styles(i),  'DisplayName', compose("u_%i", i));
+    plot(res.ids, U123(res.ids,i), compose("%so", styles(i)), 'DisplayName', compose("u_{%i, filtered}", i))
+end
+
+xlabel("index")
+ylabel("phase voltages (V)")
+
+hold off;
+legend();
+exportgraphics(fig, 'figures/Rs_voltage.png', 'Resolution', 300);
+
+
+fig = figure(102);
+fig.Position = [100 100 800 600]; 
+hold on;
+for i = 1:3
+    plot(I123(:,i),  styles(i),  'DisplayName', compose("i_%i", i));
+    plot(res.ids, I123(res.ids,i), compose("%so", styles(i)), 'DisplayName', compose("i_{%i, filtered}", i))
+end
+
+xlabel("index")
+ylabel("phase currents (A)")
+
+hold off;
+legend();
+exportgraphics(fig, 'figures/Rs_current.png', 'Resolution', 300);
+
+
+Rphase = zeros(1, 3);
+
+fig = figure(103);
+fig.Position = [100 100 800 600]; 
+hold on;
+for i = 1:3
+    R123_=R123(:, i);
+
+    plot(R123_, compose("%so", styles(i)),  'DisplayName', compose("R_%i", i));
+
+ 	R123_ = R123_(~isnan(R123_));
+    Rphase(i) = mean(R123_);
+
+    plot(Rphase(i)*ones(1, numel(R123(:, i))), styles(i), 'DisplayName', compose("R_{%i, mean}", i));
+end
+
+Rphase
+mean(Rphase)
+
+plot(mean(Rphase)*ones(1, numel(R123(:, i))), "c", 'DisplayName', "R_{mean}");
+
+xlabel("index")
+ylabel("phase resistance (Ohm)")
+
+hold off;
+legend();
+
+
+exportgraphics(fig, 'figures/Rs_resistance.png', 'Resolution', 300);
 
 
 
