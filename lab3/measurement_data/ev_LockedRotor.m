3cfa8a0a6dea7ae21b31c7d2991a3da0ec494fec
Matthias Meinhart <matthias.meinhart@racing.tugraz.at>
2025-12-29 12:54:10 +0100

    locked rotor eval

--- lab3/measurement_data/ev_LockedRotor.m
+++ lab3/measurement_data/ev_LockedRotor.m
@@ -6,7 +6,7 @@ fname={'lr_30Hz.txt', ...
         'lr_40Hz.txt', ...
         'lr_50Hz.txt'};
 
-wahl = 3;
+wahl = 1;
 
 mw=load(fname{wahl});
 
@@ -86,8 +86,8 @@ grid on; zoom on;
 xlabel('Strom, Grundschwingungseffektivwert in A');
 ylabel('Spannung, Grundschwingungseffektivwert in V');
 
-
-figure(302);
+%%
+figure(302);clf;
 %plot(res.avg(:,2),res.avg(:,1),'-bo');
 plot(Ism,Rkm,'o');
 grid on; zoom on;
@@ -96,7 +96,7 @@ ylabel('Rk in Ohm');
 
 
 
-figure(303);
+figure(303);clf;
 %plot(res.avg(:,2),res.avg(:,1),'-bo');
 plot(Ism,Lkm,'o');
 grid on; zoom on;
@@ -104,4 +104,101 @@ xlabel('Strom, Grundschwingungseffektivwert in A');
 ylabel('Lk in H');
 
 
+close all
 %% grouped plots
+clc
+
+res_files = ["lr_30Hz_LR.mat"; "lr_40Hz_LR.mat"; "lr_50Hz_LR.mat"];
+colors = ["r", "g", "b"];
+
+% Rk
+figure(400), clf;
+hold on
+for i = 1:3
+    clear("Ism", "Rkm", "Lkm")
+
+    load(res_files(i))
+
+    plot(Ism,Rkm,"o", 'Color',colors(i)) 
+    Rkm_avgs(i) = mean(rmmissing(Rkm));
+    yline(Rkm_avgs(i),"--", 'Color',colors(i))
+end
+hold off
+grid on
+xlabel('Current, RMS of fundamental wave in A');
+ylabel('R_k in \Omega');
+legend(["30 Hz", "30 Hz mean", "40 Hz", "40 Hz mean", "50 Hz", "50 Hz mean"])
+
+disp("Rkm averages \\ avg of avgs")
+disp(Rkm_avgs);
+disp(mean(Rkm_avgs))
+
+% Lk
+figure(401), clf;
+hold on
+for i = 1:3
+    clear("Ism", "Rkm", "Lkm")
+
+    load(res_files(i))
+
+    plot(Ism,Lkm,"o", 'Color',colors(i)) 
+    Lkm_avgs(i) = mean(rmmissing(Lkm));
+    yline(Lkm_avgs(i),"--", 'Color',colors(i))
+end
+hold off
+grid on
+xlabel('Current, RMS of fundamental wave in A');
+ylabel('L_k in H');
+legend(["30 Hz", "30 Hz mean", "40 Hz", "40 Hz mean", "50 Hz", "50 Hz mean"])
+
+disp("Rkm averages \\ avg of avgs")
+disp(Lkm_avgs);
+disp(mean(Lkm_avgs))
+
+f = [30, 40, 50];
+
+
+%% extrapolate to 0
+
+x_tl = -5:1:55;
+
+% Rk
+pR = polyfit(f, Rkm_avgs, 1);
+Rk_tl = pR(1)*x_tl + pR(2);
+Rk0 = Rk_tl(x_tl == 0)
+
+figure(500),clf;
+hold on
+for i = 1:3
+    plot(f(i),Rkm_avgs(i),"o", 'Color',colors(i)) 
+end
+plot(x_tl, Rk_tl, "--", 'Color', [0,0,0]);
+plot(0, Rk0, "x", 'Color', [0,0,0]);
+hold off
+grid on
+xlabel('Frequency in Hz');
+ylabel('R_k in \Omega');
+legend(["30 Hz", "40 Hz", "50 Hz", "trendline", "extrapolated 0 Hz"])
+
+%  Lk
+pL = polyfit(f, Lkm_avgs, 1);
+Lk_tl = pL(1)*x_tl + pL(2);
+Lk0 = Lk_tl(x_tl == 0)
+
+figure(501),clf;
+hold on
+for i = 1:3
+    plot(f(i),Lkm_avgs(i),"o", 'Color',colors(i)) 
+end
+plot(x_tl, Lk_tl, "--", 'Color', [0,0,0]);
+plot(0, Lk0, "x", 'Color', [0,0,0]);
+hold off
+grid on
+xlabel('Frequency in Hz');
+ylabel('L_k in H');
+legend(["30 Hz", "40 Hz", "50 Hz", "trendline", "extrapolated 0 Hz"])
+
+
+
+T = table([f'; 0] , [Lkm_avgs'; Lk0], [Rkm_avgs'; Rk0])
+table2latex(T)
