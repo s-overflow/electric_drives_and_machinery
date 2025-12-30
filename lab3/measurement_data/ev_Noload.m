9c72d4350c55b7dd01611908982aa7399388c2be
s-overflow <polt.simon@gmx.at>
2025-12-30 14:09:34 +0100

    small adaptions at noload eval

--- lab3/measurement_data/ev_Noload.m
+++ lab3/measurement_data/ev_Noload.m
@@ -5,19 +5,21 @@
 %     Kanäle 2,   3    ,6
 
 fname='nl_50Hz.txt';
-
-
 mw=load(fname);
 
-% config: config_IM_lockedrotor_235_211210.txt
-%     FUNC "VOLT2:HAR","VOLT3:HAR","VOLT6:HAR","CURR2:HAR","CURR3:HAR","CURR6:HAR","POW2:HAR","POW3:HAR","POW6:HAR","FREQ"
-% hh mm ss    Ua         Ub            Uc
-%  1  2  3    4           5            6           7           8           9            10        11         12       13  %
+% --- Export folder
+outDir = 'figures';
+if ~exist(outDir, 'dir')
+    mkdir(outDir);
+end
+
+% --- Helper: export a figure by number with a given name
+saveFig = @(figNo, name) exportgraphics(figure(figNo), ...
+    fullfile(outDir, name + ".png"), 'Resolution', 300);
 
 % --- Known parameters (from locked rotor test etc.)
-Lsigs = 3.992710335861623e-04;   % stator leakage inductance (H)
+Lsigs = 0.6897e-03;   % stator leakage inductance (H)
 Rs    = 0.1995;                  % stator resistance (Ohm)
-
 
 % --- Read measured fundamental harmonics
 Us = mean(mw(:,[4 5 6]),2);      % mean phase voltage magnitude (V)
@@ -30,14 +32,11 @@ ws = 2*pi*fs;                    % electrical angular frequency
 % --- Power factor cos(phi)
 cphi=Ps./(3*Us.*Is);
 
-% avoid numerical issues
-% cphi = max(min(cphi,1),-1);
-
 phi = acos(cphi);                % phase angle between U and I
 
 % --- Complex phasors (choose Us as reference angle 0)
-Vs = Us .* exp(1j*0);            % complex voltage phasor
-IsC = Is.*exp(-1j*acos(cphi)); %komplexer Zeiger
+Vs  = Us .* exp(1j*0);           % complex voltage phasor
+IsC = Is .* exp(-1j*acos(cphi)); % komplexer Zeiger
 
 % --- No-load impedance
 Znl = Vs ./ IsC;                 % complex impedance
@@ -46,7 +45,7 @@ Xnl = imag(Znl);
 
 % --- Remove stator series elements: Rs + j*w*Lsigs
 Zseries = Rs + 1j*ws*Lsigs;
-Zm = Znl - Zseries;              % magnetizing branch impedance (parallel of RFe and j*w*Lm)
+Zm = Znl - Zseries;              % magnetizing branch impedance
 
 Rm = real(Zm);
 Xm = imag(Zm);
@@ -64,49 +63,48 @@ Lm = -1 ./ (ws .* Bm);
 
 % --- Magnetizing voltage and currents
 Vm  = abs(IsC .* Zm);            % |Vm| = |I|*|Zm|
-
 Ife = Vm ./ RFe;                 % iron loss current
 Im  = Vm ./ (ws .* Lm);          % magnetizing current (reactive part)
 
 % --- Magnetizing flux linkage magnitude (Lambda_m)
 Lambda_m = Vm ./ ws;             % because |Vm| = ws * |Lambda_m|
 
+% ---------------- Figures + Export ----------------
 
-
-figure(200);
+figure(200); clf
 plot(Is,Us,'o-');
 xlabel("Stator current |Is| [A]");
 ylabel("Phase voltage |Us| [V]");
 grid on; zoom on;
+saveFig(200, "Is_vs_Us");
 
-figure(201);
+figure(201); clf
 plot(Is,Ps,'o-');
 xlabel("Stator current |Is| [A]");
 ylabel("3-phase active power Ps [W]");
 grid on; zoom on;
+saveFig(201, "Is_vs_Ps");
 
-
-figure(202);
+figure(202); clf
 plot(Im, Lambda_m,'o-');
 xlabel("Magnetizing current |Im| [A]");
 ylabel("Magnetizing flux linkage |Lambda_m| [Vs]");
-title("Magnetizing curve: Lambda_m vs Im");
 grid on; zoom on;
+saveFig(202, "Im_vs_Lambda_m");
 
-figure(203);
+figure(203); clf
 plot(Im, Lm,'o-');
 xlabel("Magnetizing current |Im| [A]");
 ylabel("Magnetizing inductance Lm [H]");
-title("Magnetizing inductance vs Im (saturation effect)");
 grid on; zoom on;
+saveFig(203, "Im_vs_Lm");
 
-figure(204);
+figure(204); clf
 plot(Is, RFe,'o-');
 xlabel("Stator current |Is| [A]");
 ylabel("Iron loss resistance RFe [Ohm]");
-title("Iron loss resistance vs current");
 grid on; zoom on;
-
+saveFig(204, "Is_vs_RFe");
 
 % --- Reference magnetizing inductance at Lambda_m_ref = 0.08 Vs
 Lambda_m_ref = 0.08;   % [Vs]
@@ -123,5 +121,3 @@ Lm_ref = interp1(Lambda_unique, Lm_unique, Lambda_m_ref, 'linear', 'extrap');
 
 disp("========== REFERENCE MAGNETIZING INDUCTANCE ==========");
 fprintf("Lm_ref at Lambda_m_ref = %.3f Vs: %.6e H\n", Lambda_m_ref, Lm_ref);
-
-
