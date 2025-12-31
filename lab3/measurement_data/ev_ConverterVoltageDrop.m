ea3da66938b185ff114f276852226d59202cc319
Matthias Meinhart <matthias.meinhart@racing.tugraz.at>
2025-12-31 15:49:45 +0100

    close to solving, issue with fitting v0 from N5000

--- lab3/measurement_data/ev_ConverterVoltageDrop.m
+++ lab3/measurement_data/ev_ConverterVoltageDrop.m
@@ -58,39 +58,39 @@ db=mv.Y(8).Data';
 dc=mv.Y(9).Data';
 
 
-    
-    % abc -> alpha, beta
-    T=2/3*[1 -.5 -.5
-           0 sqrt(3)/2 -sqrt(3)/2
-           .5 .5 .5];
-    
-    % space vector
-    isS=(T*[ia ib -ia-ib]')';
-    is=sqrt(isS(:,1).^2+isS(:,2).^2);
-    
-    phii=atan2(isS(:,2),isS(:,1));
 
-    % phii and t gets overwritten in line ~160
-    
-    % get stator frequency, but isnt used?
-    phiiu=unwrap(phii); 
-    gi=polyfit(t,phiiu,1);
-    ws=gi(1);
-    fs=ws/(2*pi)
+% abc -> alpha, beta
+T=2/3*[1 -.5 -.5
+       0 sqrt(3)/2 -sqrt(3)/2
+       .5 .5 .5];
 
-    % figure(333)
-    % hold on
-    % plot(t, phiiu)
-    % plot(t, ws*t + gi(2))
-    % hold off
-    % legend(["phii unwrapped", "linear fit"])
-    % xlabel("t in s")
-    % ylabel("phi / rad")
-    
-    vsS=(T*[va vb vc]')';
-    vs=sqrt(vsS(:,1).^2+vsS(:,2).^2);
-    
-    
+% space vector
+isS=(T*[ia ib -ia-ib]')';
+is=sqrt(isS(:,1).^2+isS(:,2).^2);
+
+phii=atan2(isS(:,2),isS(:,1));
+
+% phii and t gets overwritten in line ~160
+
+% get stator frequency, but isnt used?
+phiiu=unwrap(phii); 
+gi=polyfit(t,phiiu,1);
+ws=gi(1);
+fs=ws/(2*pi)
+
+% figure(333)
+% hold on
+% plot(t, phiiu)
+% plot(t, ws*t + gi(2))
+% hold off
+% legend(["phii unwrapped", "linear fit"])
+% xlabel("t in s")
+% ylabel("phi / rad")
+
+vsS=(T*[va vb vc]')';
+vs=sqrt(vsS(:,1).^2+vsS(:,2).^2);
+
+
 %     nn=20;
 %     bb=ones(nn,1);
 %     aa=zeros(nn,1);
@@ -99,58 +99,103 @@ dc=mv.Y(9).Data';
 %     vsSf=filtfilt(bb,aa,vsS);
 %     vsSf=filtfilt(bb,aa,vsS);
 %     
-    
-    
-    figure(1);
-    plot(t,[va vb vc]);
-    grid on;
-    xlabel('Time in s');
-    ylabel('Stator reference voltages in V');
-    legend('va','vb','vc','Location','SouthEast');
-    
-    
-    figure(2);
-    plot(t,[ia ib -(ia+ib)]);
-    grid on;
-    xlabel('Time in s');
-    ylabel('Statorcurrents in A');
-    legend('ia','ib','ic','Location','SouthEast');
-    
-    
-    figure(3);
-    plot(t,phiel);
-    grid on;
-    xlabel('Time in s');
-    ylabel('Electr. rotorposition in rad');
-    
-    
-    figure(4);
-    plot(t,torq);
-    grid on;
-    xlabel('Time in s');
-    ylabel('Shafttorque in Nm');
-    
-    figure(5);
-    plot(t,Vdc,t,Vdcfilt);
-    grid on;
-    xlabel('Time in s');
-    ylabel('Vdc in V');
-    legend('unfiltered','filtered','Location','best');
-    
-    
-    
-    figure(6);
-    plot(t,wfilt*30/pi);
-    grid on;
-    xlabel('Time in s');
-    ylabel('Speed in rpm');
-    
-    figure(7);
-    plot(ib,vb);
-    grid on;
-    xlabel('Time in s');
-    ylabel('vb in V');
 
+
+figure(1);
+plot(t,[va vb vc]);
+grid on;
+xlabel('Time in s');
+ylabel('Stator reference voltages in V');
+legend('va','vb','vc','Location','SouthEast');
+
+
+figure(2);
+plot(t,[ia ib -(ia+ib)]);
+grid on;
+xlabel('Time in s');
+ylabel('Statorcurrents in A');
+legend('ia','ib','ic','Location','SouthEast');
+
+
+% figure(3);
+% plot(t,phiel);
+% grid on;
+% xlabel('Time in s');
+% ylabel('Electr. rotorposition in rad');
+
+
+% figure(4);
+% plot(t,torq);
+% grid on;
+% xlabel('Time in s');
+% ylabel('Shafttorque in Nm');
+
+figure(5);
+plot(t,Vdc,t,Vdcfilt);
+grid on;
+xlabel('Time in s');
+ylabel('Vdc in V');
+legend('unfiltered','filtered','Location','best');
+
+
+
+% figure(6);
+% plot(t,wfilt*30/pi);
+% grid on;
+% xlabel('Time in s');
+% ylabel('Speed in rpm');
+
+%% get viref_per_ii dSpace data
+% vabc_ref = [va vb vc];
+% iabc_ref = [ia ib ic];
+% 
+% sel = mod(wahl,3) + 1;
+% vi_ref = vabc_ref(:, sel);
+% i_sel = iabc_ref(:, sel);
+% 
+% i_threshold = 5;
+% vir_lin1 = vi_ref(i_sel > i_threshold);
+% i_lin1 = i_sel(i_sel > i_threshold);
+% p1=polyfit(i_lin1,vir_lin1,1);
+% 
+% vir_lin2 = vi_ref(i_sel < -i_threshold);
+% i_lin2 = i_sel(i_sel < -i_threshold);
+% p2=polyfit(i_lin2,vir_lin2,1);
+% 
+% gradients = [p1(1), p2(1)];
+% pg_diff = diff(gradients)
+% allowed_diff = 1e-2;
+% if(max(abs(pg_diff)) > allowed_diff)
+%     warning("vi_ref fit: max gradient diff: " + max(abs(pg_diff)) + " > " +allowed_diff);
+% end
+% 
+% switch wahl
+%     % ia = 0, sel = b
+%     case 1
+%        vbref_per_ib = mean(gradients);
+%        save("cvd_calc.mat", "vbref_per_ib");
+%     % ib = 0, sel = c
+%     case 2
+%         vcref_per_ic = mean(gradients);
+%         save("cvd_calc.mat", "vcref_per_ic", "-append");
+%     % ic = 0, sel = a
+%     case 3
+%         varef_per_ia = mean(gradients);
+%         save("cvd_calc.mat", "varef_per_ia", "-append");
+% end
+% 
+% i_help = min(i_sel):1:max(i_sel);
+% figure(7),clf;
+% hold on
+% plot(i_sel,vi_ref);
+% plot(i_lin1, p1(1)*i_lin1 + p1(2),"--", "LineWidth",1.3)
+% plot(i_lin2, p2(1)*i_lin2 + p2(2),"--", "LineWidth",1.3)
+% plot(i_help, mean(gradients)*i_help,"-.", "LineWidth",1.3)
+% hold off
+% grid on;
+% legend(["measured", "linear region+ fit", "linear region- fit", "mean fit gradient"])
+% xlabel('i-ref in A');
+% ylabel('vi-ref in V');
     
     %% N5000 Messwerte
     mw=mwgeigl(pwd,fname);
@@ -214,4 +259,79 @@ dc=mv.Y(9).Data';
     end;
     
     
-    
+%% get vi_per_ii and v0_per_i N5000 data
+sel = mod(wahl,3) + 1;
+vi = uabc(:, sel);
+i_sel = iabc(:, sel);
+
+i_threshold = 5;
+vir_lin1 = vi(i_sel > i_threshold);
+vi0_lin1 = u0(i_sel > i_threshold);
+i_lin1 = i_sel(i_sel > i_threshold);
+p1=polyfit(i_lin1,vir_lin1,1);
+p01 = polyfit(i_lin1,vi0_lin1,1);
+
+vir_lin2 = vi(i_sel < -i_threshold);
+vi0_lin2 = u0(i_sel < -i_threshold);
+i_lin2 = i_sel(i_sel < -i_threshold);
+p2=polyfit(i_lin2,vir_lin2,1);
+p02 = polyfit(i_lin2,vi0_lin2,1);
+
+gradients_IM = [p1(1), p2(1)];
+pg_diff_IM = diff(gradients_IM)
+allowed_diff = 1e-2;
+if(max(abs(pg_diff_IM)) > allowed_diff)
+    warning("vi_IM fit: max gradient diff: " + max(abs(pg_diff_IM)) + " > " +allowed_diff);
+end
+
+gradients_0 = [p02(1)];
+pg_diff_0 = diff(gradients_0)
+if(max(abs(pg_diff_0)) > allowed_diff)
+    warning("vi_0 fit: max gradient diff: " + max(abs(pg_diff_0)) + " > " +allowed_diff);
+end
+
+switch wahl
+    % ia = 0, sel = b
+    case 1
+       vb_per_ib = mean(gradients_IM);
+       v0_per_ib = mean(gradients_0);
+       save("cvd_calc.mat", "vb_per_ib","v0_per_ib", "-append");
+    % ib = 0, sel = c
+    case 2
+        vc_per_ic = mean(gradients_IM);
+        v0_per_ic = mean(gradients_0);
+        save("cvd_calc.mat", "vc_per_ic","v0_per_ic","-append");
+    % ic = 0, sel = a
+    case 3
+        va_per_ia = mean(gradients_IM);
+        v0_per_ia = mean(gradients_0);
+        save("cvd_calc.mat", "va_per_ia","v0_per_ia", "-append");
+end
+
+phases_str = ["a", "b", "c"];
+i_help = min(i_sel):1:max(i_sel);
+figure(20),clf;
+hold on
+plot(i_sel,vi);
+plot(i_lin1, p1(1)*i_lin1 + p1(2),"--", "LineWidth",1.3)
+plot(i_lin2, p2(1)*i_lin2 + p2(2),"--", "LineWidth",1.3)
+plot(i_help, mean(gradients_IM)*i_help,"-.", "LineWidth",1.3)
+hold off
+grid on;
+legend(["measured", "linear region+ fit", "linear region- fit", "mean fit gradient"])
+xlabel('ii in A');
+ylabel('vi-IM in V');
+title("Phase "+ phases_str(sel))
+
+figure(21),clf;
+hold on
+plot(i_sel,vi);
+plot(i_lin1, p01(1)*i_lin1 + p01(2),"--", "LineWidth",1.3)
+plot(i_lin2, p02(1)*i_lin2 + p02(2),"--", "LineWidth",1.3)
+plot(i_help, mean(gradients_0)*i_help,"-.", "LineWidth",1.3)
+hold off
+grid on;
+legend(["measured", "linear region+ fit", "linear region- fit", "mean fit gradient"])
+xlabel('ii in A');
+ylabel('vi-0 in V');
+title("Star point voltage for measurement "+ phases_str(sel))
